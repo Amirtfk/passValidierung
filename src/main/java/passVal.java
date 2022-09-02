e572a1b121f1323302a48993b5369900564fcc5e
Amir Tafkhimi <amir.tafkhimi@gmail.com>
2022-09-02 15:11:18 +0200

    Add PassNumber

--- src/main/java/passVal.java
+++ src/main/java/passVal.java
@@ -1,6 +1,7 @@
 public class passVal {
     public static void main(String[] args) {
         System.out.println(lengthOfPass("test Pass"));
+        String password = "123456789";
     }
 
     public static String lengthOfPass(String password) {
@@ -12,8 +13,13 @@ public class passVal {
     }
     }
 
-    public static String passNumber (String number) {
-
+    public static boolean passNumber (String password) {
+        for (int i = 0; i < password.length(); i++){
+            if(Character.isDigit(password.charAt(i))){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
--- src/test/java/passValTest.java
+++ src/test/java/passValTest.java
@@ -1,13 +1,13 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class passValTest {
 
     @Test
     void passwordLengthValid(){
 
-    //GIVEN
+    // GIVEN
     String passwordLength = "12345678";
     // WHEN
     String actual = passVal.lengthOfPass(passwordLength);
@@ -19,7 +19,7 @@ public class passValTest {
     @Test
     void passwordLengthUnvalid(){
 
-        //GIVEN
+        // GIVEN
         String passwordLength = "12345";
         // WHEN
         String actual = passVal.lengthOfPass(passwordLength);
@@ -29,4 +29,27 @@ public class passValTest {
 
 
 
+    @Test
+    void passNumberIs(){
 
+        // GIVEN
+        String passZahl = "0123456789";
+        // WHEN
+        Boolean actual = passVal.passNumber(passZahl);
+        // THEN
+        assertTrue(actual);
+        //assertEquals("true", actual);
+    }
+
+    @Test
+    void passNumberIsNot(){
+
+        // GIVEN
+        String passZahl = "";
+        // WHEN
+        Boolean actual = passVal.passNumber(passZahl);
+        // THEN
+        assertFalse(actual);
+        //assertEquals("true", actual);
+    }
+}
